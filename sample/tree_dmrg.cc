cd96278c8b8b1daa5d308ca6cdb524a2d7ae897d
Nils Strand <nes227@bismuth.theory.northwestern.edu>
2020-09-23 11:52:18 -0500

    arnoldi might be wrongly implemented

--- sample/tree_dmrg.cc
+++ sample/tree_dmrg.cc
@@ -1,6 +1,7 @@
 #include "itensor/all.h"
 #include "itensor/ttn/binarytree.h"
 #include "itensor/ttn/tree_dmrg.h"
+#include "itensor/util/print_macro.h"
 
 using namespace itensor;
 
@@ -24,7 +25,7 @@ void printVect(std::vector <int> const &a) {
 int main()
 {
 
-  int N = 32;
+  int N = 8;
   auto sites = SpinHalf(N,{"ConserveQNs",false});
 
   auto state = InitState(sites);
@@ -44,6 +45,13 @@ int main()
   // auto H = toMPO(ampo);
 
   // Real lambda = 0.0;
+  // // std::vector<Real> plist(N + 1);
+  // // std::vector<Real> qlist(N + 1);
+  // // for(auto j : range(N + 1))
+  // // {
+  // //   plist[j] = (Real)std::rand() / RAND_MAX;
+  // //   qlist[j] = (Real)std::rand() / RAND_MAX;
+  // // }
   // std::vector<Real> plist(N + 1, 0.6);
   // std::vector<Real> qlist(N + 1, 0.4);
   // auto ampo = AutoMPO(sites);
@@ -64,44 +72,47 @@ int main()
   // ampo += -plist[N], "projUp", N;
   // auto H = toMPO(ampo);
 
-  Real lambda = 0.0;
-  std::vector<Real> plist(N);
-  std::vector<Real> qlist(N);
-  for(auto j : range(N))
-  {
-    plist[j] = (Real)std::rand() / RAND_MAX;
-    qlist[j] = (Real)std::rand() / RAND_MAX;
-  }
-  // std::vector<Real> plist(N, 0.6);
-  // std::vector<Real> qlist(N, 0.4);
-  // std::vector<Real> plist({ 0.1, 0.2, 0.3, 0.4, 0.5, 0.6 });
-  // std::vector<Real> qlist({ 1.1, 1.2, 1.3, 1.4, 1.5, 1.6 });
-  auto ampo = AutoMPO(sites);
-  for(auto j : range1(N - 1))
-  {
-    ampo += plist[j - 1] * std::exp(lambda), "S+", j, "S-", j + 1;
-    ampo += -plist[j - 1], "projUp", j, "projDn", j + 1;
-    ampo += qlist[j - 1] * std::exp(-lambda), "S-", j, "S+", j + 1;
-    ampo += -qlist[j - 1], "projDn", j, "projUp", j + 1;
-  }
-  ampo += plist[N - 1] * std::exp(lambda), "S+", N, "S-", 1;
-  ampo += -plist[N - 1], "projUp", N, "projDn", 1;
-  ampo += qlist[N - 1] * std::exp(-lambda), "S-", N, "S+", 1;
-  ampo += -qlist[N - 1], "projDn", N, "projUp", 1;
-  auto H = toMPO(ampo);
+  // Real lambda = 0.0;
+  // std::vector<Real> plist(N);
+  // std::vector<Real> qlist(N);
+  // for(auto j : range(N))
+  // {
+  //   plist[j] = (Real)std::rand() / RAND_MAX;
+  //   qlist[j] = (Real)std::rand() / RAND_MAX;
+  // }
+  // // std::vector<Real> plist(N, 0.6);
+  // // std::vector<Real> qlist(N, 0.4);
+  // // std::vector<Real> plist({ 0.1, 0.2, 0.3, 0.4, 0.5, 0.6 });
+  // // std::vector<Real> qlist({ 1.1, 1.2, 1.3, 1.4, 1.5, 1.6 });
+  // auto ampo = AutoMPO(sites);
+  // for(auto j : range1(N - 1))
+  // {
+  //   ampo += plist[j - 1] * std::exp(lambda), "S+", j, "S-", j + 1;
+  //   ampo += -plist[j - 1], "projUp", j, "projDn", j + 1;
+  //   ampo += qlist[j - 1] * std::exp(-lambda), "S-", j, "S+", j + 1;
+  //   ampo += -qlist[j - 1], "projDn", j, "projUp", j + 1;
+  // }
+  // ampo += plist[N - 1] * std::exp(lambda), "S+", N, "S-", 1;
+  // ampo += -plist[N - 1], "projUp", N, "projDn", 1;
+  // ampo += qlist[N - 1] * std::exp(-lambda), "S-", N, "S+", 1;
+  // ampo += -qlist[N - 1], "projDn", N, "projUp", 1;
+  // auto H = toMPO(ampo);
 
-  // auto H = MPO_ASEP(sites, std::vector<Real>(N + 1, 0.6), std::vector<Real>(N + 1, 0.4), -0.0001);
+  auto H = MPO_ASEP(sites, std::vector<Real>(N + 1, 0.6), std::vector<Real>(N + 1, 0.4), 0.0);
   // auto H = MPO_ASEP(sites, std::vector<Real>(N, 0.6), std::vector<Real>(N, 0.4), 0.0);
   // auto H = MPO_ASEP(sites, { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8 }, { 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8 }, 0.0001);
   // auto H = MPO_ASEP(sites, { 0.1, 0.2, 0.3, 0.4 }, { 1.1, 1.2, 1.3, 1.4 }, 0.0);
 
-  // auto Hfull = H(1) * H(2) * H(3) * H(4) * H(5) * H(6);
+  // auto Hfull = H(1) * H(2) * H(3) * H(4) * H(5) * H(6) * H(7) * H(8);
   // auto inds = Hfull.inds();
-  // auto C = std::get<0>(combiner(inds[0], inds[2], inds[4], inds[6], inds[8], inds[10]));
-  // auto Cp = std::get<0>(combiner(inds[1], inds[3], inds[5], inds[7], inds[9], inds[11]));
-  // PrintData(Hfull);
-  // PrintData(C * Hfull * Cp);
-  // PrintData(H);
+  // auto C = std::get<0>(combiner(inds[0], inds[2], inds[4], inds[6], inds[8], inds[10], inds[12], inds[14]));
+  // auto Cp = std::get<0>(combiner(inds[1], inds[3], inds[5], inds[7], inds[9], inds[11], inds[13], inds[15]));
+  // auto Hfullmat = C * Hfull * Cp;
+  // Matrix M(pow2(N), pow2(N));
+  // for(auto it : iterInds(Hfullmat)) {
+  //   M(it[0].val - 1, it[1].val - 1) = Hfullmat.real(it);
+  // }
+  // Print(M);
 
   println("Construction of the MPO and LocalMPO");
 //  LocalMPO_BT PH(H,args);
@@ -110,6 +121,10 @@ int main()
   auto psi0 = BinaryTree(state);
   // PrintData(psi0);
   // auto psi0 = randomBinaryTree(sites, 100);
+
+  // auto psidag = prime(dag(psi0));
+  // std::vector<std::vector<ITensor>> MPO(psi0.height() + 2);
+
   println("Construction of the BinaryTree");
 //  PH.position(0,psi0);
 //  PH.haveBeenUpdated(2);
