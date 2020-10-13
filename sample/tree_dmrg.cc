81327f5ff5bc920e6169125a7995eaf2da59b205
Nils Strand <nes227@bismuth.theory.northwestern.edu>
2020-10-12 23:50:01 -0500

    current and occupation number tilt enabled, single runs

--- sample/tree_dmrg.cc
+++ sample/tree_dmrg.cc
@@ -22,10 +22,10 @@ void printVect(std::vector <int> const &a) {
   std::cout<< '\n';
 }
 
-int main()
+int main(int argc, char** argv)
 {
 
-  int N = 128;
+  int N = 8;
   auto sites = SpinHalf(N,{"ConserveQNs",false});
 
   auto state = InitState(sites);
@@ -44,7 +44,8 @@ int main()
   //   }
   // auto H = toMPO(ampo);
 
-  // Real lambda = 0.0;
+  // Real lc = atof(argv[1]), lo = atof(argv[2]);
+  // printfln("lc %d lo %d", lc, lo);
   // // std::vector<Real> plist(N + 1);
   // // std::vector<Real> qlist(N + 1);
   // // for(auto j : range(N + 1))
@@ -52,27 +53,33 @@ int main()
   // //   plist[j] = (Real)std::rand() / RAND_MAX;
   // //   qlist[j] = (Real)std::rand() / RAND_MAX;
   // // }
-  // std::vector<Real> plist(N + 1, 0.6);
-  // std::vector<Real> qlist(N + 1, 0.4);
+  // std::vector<Real> plist(N + 1, 0.1);
+  // std::vector<Real> qlist(N + 1, 0.9);
+  // plist[0] = plist[N] = qlist[0] = qlist[N] = 0.5;
   // auto ampo = AutoMPO(sites);
-  // ampo += plist[0] * std::exp(lambda), "S-", 1;
+  // ampo += plist[0] * std::exp(lc), "S-", 1;
   // ampo += -plist[0], "projDn", 1;
-  // ampo += qlist[0] * std::exp(-lambda), "S+", 1;
+  // ampo += qlist[0] * std::exp(-lc), "S+", 1;
   // ampo += -qlist[0], "projUp", 1;
   // for(auto j : range1(N - 1))
   // {
-  //   ampo += plist[j] * std::exp(lambda), "S+", j, "S-", j + 1;
+  //   ampo += plist[j] * std::exp(lc), "S+", j, "S-", j + 1;
   //   ampo += -plist[j], "projUp", j, "projDn", j + 1;
-  //   ampo += qlist[j] * std::exp(-lambda), "S-", j, "S+", j + 1;
+  //   ampo += qlist[j] * std::exp(-lc), "S-", j, "S+", j + 1;
   //   ampo += -qlist[j], "projDn", j, "projUp", j + 1;
   // }
-  // ampo += qlist[N] * std::exp(-lambda), "S-", N;
+  // ampo += qlist[N] * std::exp(-lc), "S-", N;
   // ampo += -qlist[N], "projDn", N;
-  // ampo += plist[N] * std::exp(lambda), "S+", N;
+  // ampo += plist[N] * std::exp(lc), "S+", N;
   // ampo += -plist[N], "projUp", N;
+  // for(auto j : range1(N))
+  // {
+  //   ampo += -lo, "projDn", j;
+  // }
   // auto H = toMPO(ampo);
 
-  // Real lambda = 0.0;
+  Real lc = atof(argv[1]), lo = atof(argv[2]);
+  printfln("lc %d lo %d", lc, lo);
   // std::vector<Real> plist(N);
   // std::vector<Real> qlist(N);
   // for(auto j : range(N))
@@ -80,34 +87,39 @@ int main()
   //   plist[j] = (Real)std::rand() / RAND_MAX;
   //   qlist[j] = (Real)std::rand() / RAND_MAX;
   // }
-  // // std::vector<Real> plist(N, 0.6);
-  // // std::vector<Real> qlist(N, 0.4);
-  // // std::vector<Real> plist({ 0.1, 0.2, 0.3, 0.4, 0.5, 0.6 });
-  // // std::vector<Real> qlist({ 1.1, 1.2, 1.3, 1.4, 1.5, 1.6 });
-  // auto ampo = AutoMPO(sites);
-  // for(auto j : range1(N - 1))
-  // {
-  //   ampo += plist[j - 1] * std::exp(lambda), "S+", j, "S-", j + 1;
-  //   ampo += -plist[j - 1], "projUp", j, "projDn", j + 1;
-  //   ampo += qlist[j - 1] * std::exp(-lambda), "S-", j, "S+", j + 1;
-  //   ampo += -qlist[j - 1], "projDn", j, "projUp", j + 1;
-  // }
-  // ampo += plist[N - 1] * std::exp(lambda), "S+", N, "S-", 1;
-  // ampo += -plist[N - 1], "projUp", N, "projDn", 1;
-  // ampo += qlist[N - 1] * std::exp(-lambda), "S-", N, "S+", 1;
-  // ampo += -qlist[N - 1], "projDn", N, "projUp", 1;
-  // auto H = toMPO(ampo);
+  // std::vector<Real> plist(N, 0.6);
+  // std::vector<Real> qlist(N, 0.4);
+  std::vector<Real> plist({ 0.1, 0.2, 0.3, 0.4/*, 0.5, 0.6, 0.7, 0.8*/ });
+  std::vector<Real> qlist({ 1.1, 1.2, 1.3, 1.4/*, 1.5, 1.6, 1.7, 1.8*/ });
+  auto ampo = AutoMPO(sites);
+  for(auto j : range1(N - 1))
+  {
+    ampo += plist[j - 1] * std::exp(lc), "S+", j, "S-", j + 1;
+    ampo += -plist[j - 1], "projUp", j, "projDn", j + 1;
+    ampo += qlist[j - 1] * std::exp(-lc), "S-", j, "S+", j + 1;
+    ampo += -qlist[j - 1], "projDn", j, "projUp", j + 1;
+  }
+  ampo += plist[N - 1] * std::exp(lc), "S+", N, "S-", 1;
+  ampo += -plist[N - 1], "projUp", N, "projDn", 1;
+  ampo += qlist[N - 1] * std::exp(-lc), "S-", N, "S+", 1;
+  ampo += -qlist[N - 1], "projDn", N, "projUp", 1;
+  for(auto j : range1(N))
+  {
+    ampo += -lo, "projDn", j;
+  }
+  auto H = toMPO(ampo);
 
-  auto H = MPO_ASEP(sites, std::vector<Real>(N + 1, 0.6), std::vector<Real>(N + 1, 0.4), 0.0);
+  // auto H = MPO_ASEP(sites, std::vector<Real>(N + 1, 0.6), std::vector<Real>(N + 1, 0.4), 0.0001);
   // auto H = MPO_ASEP(sites, std::vector<Real>(N, 0.6), std::vector<Real>(N, 0.4), 0.0);
   // auto H = MPO_ASEP(sites, { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8 }, { 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8 }, 0.0001);
   // auto H = MPO_ASEP(sites, { 0.1, 0.2, 0.3, 0.4 }, { 1.1, 1.2, 1.3, 1.4 }, 0.0);
 
-  // auto Hfull = H(1) * H(2) * H(3) * H(4) * H(5) * H(6) * H(7) * H(8);
+  // auto Hfull = H(1) * H(2) * H(3)/* * H(4) * H(5) * H(6) * H(7) * H(8)*/;
   // auto inds = Hfull.inds();
-  // auto C = std::get<0>(combiner(inds[0], inds[2], inds[4], inds[6], inds[8], inds[10], inds[12], inds[14]));
-  // auto Cp = std::get<0>(combiner(inds[1], inds[3], inds[5], inds[7], inds[9], inds[11], inds[13], inds[15]));
+  // auto C = std::get<0>(combiner(inds[0], inds[2], inds[4]/*, inds[6], inds[8], inds[10], inds[12], inds[14]*/));
+  // auto Cp = std::get<0>(combiner(inds[1], inds[3], inds[5]/*, inds[7], inds[9], inds[11], inds[13], inds[15]*/));
   // auto Hfullmat = C * Hfull * Cp;
+  // PrintData(Hfullmat);
   // Matrix M(pow2(N), pow2(N));
   // for(auto it : iterInds(Hfullmat)) {
   //   M(it[0].val - 1, it[1].val - 1) = Hfullmat.real(it);
@@ -143,9 +155,9 @@ int main()
   // Here less than 5 cutoff values are provided, for example,
   // so all remaining sweeps will use the last one given (= 1E-10).
   //
-  auto sweeps = Sweeps(20);
-  // sweeps.maxdim() = 10,20,100,100,200;
-  sweeps.maxdim() = 10,10,10,10,10,20,20,20,20,20,100,100,100,100,100,200,200,200,200,200;
+  auto sweeps = Sweeps(5);
+  sweeps.maxdim() = 10,20,30,40,50;
+  // sweeps.maxdim() = 10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300;
   sweeps.cutoff() = 1E-13;
   sweeps.niter() = 10;
   sweeps.noise() = 0.0;
