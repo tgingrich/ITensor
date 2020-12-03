a9bcc7c9f52d2084e4134f6c6da885e4ee23e5a6
Nils Strand <nes227@bismuth.theory.northwestern.edu>
2020-12-03 09:53:31 -0600

    DMRG works great with second potential but with first unstable when particle number near nbins/2

--- sample/1dflashratchet.cc
+++ sample/1dflashratchet.cc
@@ -47,14 +47,15 @@ int main(int argc, char** argv)
 
 	auto sites = SpinHalf(bins,{"ConserveQNs",true});
 	auto state = InitState(sites);
+	int count = 0;
 	for(auto i : range(bins)) // Note: sites are labelled from 1
 		{
-		if(i%(int)ceil((Real)bins/nparticles)==0) state.set(i+1,"Up");
+		if(i%(bins/nparticles)==0 && count++ < nparticles) state.set(i+1,"Up");
 		else state.set(i+1,"Dn");
 		}
 	auto psi0 = BinaryTree(state);
 
-	Real dz = 0.001;
+	Real dz = 0.0001;
 	std::vector<Real> plist1(bins), qlist1(bins), plist2(bins), qlist2(bins);
 	for(auto j : range(bins))
 		{
@@ -66,7 +67,7 @@ int main(int argc, char** argv)
     		plist2[j] += (i+1)*ratio*strength*coefs[i]*M_PI*std::cos(2*(i+1)*M_PI*j*h)/h;
     		qlist2[j] -= (i+1)*ratio*strength*coefs[i]*M_PI*std::cos(2*(i+1)*M_PI*(j+1)*h)/h;
  			}
- 		printfln("%d %f %f %f %f",j,plist1[j],qlist1[j],plist2[j],qlist2[j]);
+ 		// printfln("%d %f %f %f %f",j,plist1[j],qlist1[j],plist2[j],qlist2[j]);
 		}
 	auto ampo1m = AutoMPO(sites), ampo1p = AutoMPO(sites), ampo2m = AutoMPO(sites), ampo2p = AutoMPO(sites);
 	for(auto j : range1(bins))
@@ -89,17 +90,6 @@ int main(int argc, char** argv)
 		ampo2p += -qlist2[j-1],"projDn",j,"projUp",j%bins+1;
 		}
 	auto W1m = toMPO(ampo1m), W1p = toMPO(ampo1p), W2m = toMPO(ampo2m), W2p = toMPO(ampo2p);
-	// auto W1m = toMPO(ampo1m)
-
-	auto anop = AutoMPO(sites);
-	for(auto j : range1(bins))
-		{
-		anop += "Sz",j;
-		}
-  	auto Nop = toMPO(anop);
-	int spin = inner(psi0,Nop,psi0);
-	printfln("\nDriving frequency: %f",freq);
-	printfln("\nParticle number: %d",spin+bins/2);
 
 	// auto H = W1m;
 	// auto Hfull = H(1)*H(2)*H(3)*H(4);
@@ -109,21 +99,50 @@ int main(int argc, char** argv)
 	// auto Hfullmat = C*Hfull*Cp;
 	// PrintData(Hfullmat);
 
-	// auto sweeps = Sweeps(8);
-	// sweeps.maxdim() = 16,16,16,16,16,16,16,16;
-	// // sweeps.maxdim() = 10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300;
-	// sweeps.cutoff() = 1E-13;
-	// sweeps.niter() = 10;
-	// sweeps.noise() = 0.0;
-	// sweeps.alpha() = 0.1,0.1,0.05,0.05,0.02,0.02,0.01,0.01;
-	// // sweeps.alpha() = 0.1,0.1,0.1,0.1,0.1,0.05,0.05,0.05,0.05,0.05,0.02,0.02,0.02,0.02,0.02,0.01,0.01,0.01,0.01,0.01,0.005,0.005,0.005,0.005,0.005,0.002,0.002,0.002,0.002,0.002;
-	// println();
-	// println(sweeps);
+	auto anop = AutoMPO(sites);
+	for(auto j : range1(bins))
+		{
+		anop += "Sz",j;
+		}
+  	auto Nop = toMPO(anop);
+	int spin = inner(psi0,Nop,psi0);
+	printfln("Driving frequency: %f",freq);
+	printfln("\nParticle number: %d",spin+bins/2);
 
-	// println("\nStart DMRG");
+	// auto expH1m = toExpH(ampo1m,1/(2*freq)), expH1p = toExpH(ampo1p,1/(2*freq)), expH2m = toExpH(ampo2m,1/(2*freq)), expH2p = toExpH(ampo2p,1/(2*freq));
+	// auto Cm = nmultMPO(prime(expH1m),expH2m,{"MaxDim",500,"Cutoff",1E-13}), Cp = nmultMPO(prime(expH1p),expH2p,{"MaxDim",500,"Cutoff",1E-13});
+	// PrintData(expH1m);
+	// PrintData(expH2m);
+	// PrintData(Cm);
 
-	// auto [energym,psim] = tree_dmrg(W1m,psi0,sweeps,{"NumCenter",2,"Order","PostOrder","Quiet",true,"WhichEig","LargestReal"});
-	// auto [energyp,psip] = tree_dmrg(W1p,psi0,sweeps,{"NumCenter",2,"Order","PostOrder","Quiet",true,"WhichEig","LargestReal"});
+	auto sweeps = Sweeps(30);
+	sweeps.maxdim() = 10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300;
+	sweeps.cutoff() = 1E-13;
+	sweeps.niter() = 10;
+	sweeps.noise() = 0.0;
+	sweeps.alpha() = 0.1,0.1,0.1,0.1,0.1,0.05,0.05,0.05,0.05,0.05,0.02,0.02,0.02,0.02,0.02,0.01,0.01,0.01,0.01,0.01,0.005,0.005,0.005,0.005,0.005,0.002,0.002,0.002,0.002,0.002;
+	println();
+	println(sweeps);
+
+	println("\nStart DMRG");
+
+	auto psi2m = std::get<1>(tree_dmrg(W2m,psi0,sweeps,{"NumCenter",2,"Order","PostOrder","Quiet",true,"WhichEig","LargestReal"}));
+
+	// auto psi2mfull = psi2m(0)*psi2m(1)*psi2m(2)*psi2m(3)*psi2m(4)*psi2m(5)*psi2m(6)*psi2m(7)*psi2m(8)*psi2m(9)*psi2m(10)*psi2m(11)*psi2m(12)*psi2m(13)*psi2m(14);
+	// auto inds2m = psi2mfull.inds();
+	// auto C2m = std::get<0>(combiner(inds2m[0],inds2m[1],inds2m[2],inds2m[3],inds2m[4],inds2m[5],inds2m[6],inds2m[7],inds2m[8],inds2m[9],inds2m[10],inds2m[11],inds2m[12],inds2m[13],inds2m[14],inds2m[15]));
+	// auto psi2mfullmat = C2m*psi2mfull;
+	// PrintData(psi2mfullmat);
+
+	auto psi2p = std::get<1>(tree_dmrg(W2p,psi0,sweeps,{"NumCenter",2,"Order","PostOrder","Quiet",true,"WhichEig","LargestReal"}));
+
+	// auto psi2pfull = psi2p(0)*psi2p(1)*psi2p(2)*psi2p(3)*psi2p(4)*psi2p(5)*psi2p(6)*psi2p(7)*psi2p(8)*psi2p(9)*psi2p(10)*psi2p(11)*psi2p(12)*psi2p(13)*psi2p(14);
+	// auto inds2p = psi2pfull.inds();
+	// auto C2p = std::get<0>(combiner(inds2p[0],inds2p[1],inds2p[2],inds2p[3],inds2p[4],inds2p[5],inds2p[6],inds2p[7],inds2p[8],inds2p[9],inds2p[10],inds2p[11],inds2p[12],inds2p[13],inds2p[14],inds2p[15]));
+	// auto psi2pfullmat = C2p*psi2pfull;
+	// PrintData(psi2pfullmat);
+
+	// auto [energym,psim] = tree_dmrg(Cm,psi0,sweeps,{"NumCenter",2,"Order","PostOrder","Quiet",true,"WhichEig","LargestReal"});
 
 	auto finish = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> elapsed = finish - start;
